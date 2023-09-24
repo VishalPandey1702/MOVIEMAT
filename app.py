fd53c8c8d3072fd77e32589c00c8a24f564f6297
Vishal_Pandey17 <106879568+VishalPandey1702@users.noreply.github.com>
2023-09-24 12:37:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import main
 import streamlit as st 
 
-
-movie_list  = main.recommend('Avatar')
-print(movie_list)
-
 st.title("Movie Recommender System")
 
 movie_name = st.text_input("Movie Name ")
